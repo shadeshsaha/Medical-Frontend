44012af5dc4142c07d22de65155c6145587084d2
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-20 01:52:56 +0600

    Updated AntD Registry and wrap component with Providers

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,35 +1,14 @@
 "use client";
+
 import store from "@/Redux/store";
-// import { ConfigProvider } from "antd";
 import { Provider } from "react-redux";
-import StyledComponentsRegistry from "./AntResistry";
+import StyledComponentsRegistry from "./AntDRegistry";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      {/* <ConfigProvider
-        theme={{
-          token: {
-            // Seed Token
-            // colorPrimary: "red",
-            borderRadius: 8,
-            colorText: "#000",
-            fontFamily: "default",
-            colorBgSpotlight: "red",
-            // Alias Token
-            colorBgContainer: "#fff",
-          },
-        }}
-      >
-        <Provider store={store}>
-          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-        </Provider>
-      </ConfigProvider> */}
-
-      <Provider store={store}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </Provider>
   );
 };
 
